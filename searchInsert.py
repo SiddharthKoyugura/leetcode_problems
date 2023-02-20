5058f259a45c217c97724532c846472106fd2789
SiddharthKoyugura <koyugurasiddhardha@gmail.com>
2023-02-21 01:09:20 +0530

    searchInsert

---
+++ searchInsert.py
@@ -0,0 +1,31 @@
+#### Approach 1:
+
+# def searchInsert(nums, target):
+#     n = len(nums)
+#     for i in range(n):
+#         if (nums[i]==target) or (nums[i]>target):
+#             return i
+#     return n
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+#########################################################################################################
+
+#### Approach 2:
+def searchInsert(nums, target):
+    n = len(nums)
+    if n==0: return 0
+    low, high, mid = 0, n-1, 0
+    while low<=high:
+        mid = (low+high)//2
+        x = nums[mid]
+        if(x==target): return mid
+        elif(x>target): high = mid-1
+        elif(x<target): low = mid+1
+
+    if nums[mid]>target: return mid
+    return mid+1
+
+# Time Complexity: O(logn)
+# Space Complexity: O(1)
